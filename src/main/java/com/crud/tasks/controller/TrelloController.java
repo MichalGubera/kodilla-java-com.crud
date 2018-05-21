676543026cddb42d9b9f503c3431be69dde606bc
MichalGubera <m.gubera@gmail.com>
2018-05-21 17:47:41 +0200

    Beginning module 18.3

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -2,11 +2,14 @@ package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.trello.client.TrelloClient;
+import org.hibernate.event.spi.SaveOrUpdateEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sound.midi.Soundbank;
+import java.net.SocketPermission;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -19,6 +22,14 @@ public class TrelloController {
     private TrelloClient trelloClient;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
+    public void getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+
+        trelloBoards.forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+
+    }
+
 //    public void getTrelloBoards() {
 //
 //        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards() //<--- task 18.2.3
@@ -32,13 +43,15 @@ public class TrelloController {
 //
 //    }
 
-    public Optional getTrelloBoards() {  //<--- task 18.2.4
-
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards()
-                .stream()
-                .filter(board -> board.getName().contains("Kodilla"))
-                .collect(Collectors.toList());
-
-        return Optional.of(trelloBoards);
-    }
+//    public Optional <List<TrelloBoardDto>> getTrelloBoards() {  //<--- task 18.2.4
+//
+//        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards()
+//                .stream()
+//                .filter(board -> board.getName().contains("Kodilla"))
+//                .forEach(System.out.println(.getId() + " " + .getName()))
+//                .collect(Collectors.toList());
+//
+//        return Optional.of(trelloBoards)
+//
+//    }
 }
