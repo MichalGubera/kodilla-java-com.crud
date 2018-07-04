71fbf95766e751167ac313e66d93d26ca86493f8
MichalGubera <m.gubera@gmail.com>
2018-07-04 18:52:45 +0200

    Add module 24.3 implement conditional instructions into mail template

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+
 @Service
 public class MailCreatorService {
 
@@ -18,11 +20,18 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+        ArrayList<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your task");
+        functionality.add("Provides connection with Trello account");
+        functionality.add("Application allows sending task to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/crud");
         context.setVariable("button", "Visit website");
-        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
         context.setVariable("goodbye_message", "Goodbye. Until the next time.");
         context.setVariable("company_name", "${info.company.name}");
         context.setVariable("company_goal", "${info.company.goal}");
